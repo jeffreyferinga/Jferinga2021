d5657a4fb58bc6e69a0ba03b51daa385b12746d6
jeffreyferinga <77978639+jeffreyferinga@users.noreply.github.com>
2021-02-16 19:19:53 -0500

    Add files via upload

---
+++ Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,12 @@
+// Write Your JavaScript Code Here
+var stuname;
+var pets;
+var funfact;
+
+stuname= "jeff";
+pets= 3;
+funfact= "i have a family of 9"
+
+console.log("My name is " + stuname )
+console.log("I have " + pets + " pets" )
+console.log("My fun fact is " + funfact)
